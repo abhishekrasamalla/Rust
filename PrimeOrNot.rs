2da7b4a9cb235dfbc415230eb8f72a347369e04e
Abhishek Rasamalla <107439350+abhishekrasamalla@users.noreply.github.com>
2024-04-23 12:18:10 +0530

    Add files via upload

---
+++ PrimeOrNot.rs
@@ -0,0 +1,23 @@
+fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false; // Numbers less than or equal to 1 are not prime
+    }
+    
+    for i in 2..=(n as f64).sqrt() as u64 {
+        if n % i == 0 {
+            return false; // Found a divisor, so n is not prime
+        }
+    }
+    
+    true // No divisors found, so n is prime
+}
+
+fn main() {
+    let num = 23;
+    
+    if is_prime(num) {
+        println!("{} is prime.", num);
+    } else {
+        println!("{} is not prime.", num);
+    }
+}
